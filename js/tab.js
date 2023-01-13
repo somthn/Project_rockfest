bc94e4c4906ea716fd8006575609c09e708f6c15
somthn <somthna0@gmail.com>
2023-01-14 02:59:07 +0900

    1. 탭메뉴 내부 css 수정 2. 탭메뉴 코드 수정 3. 외부 링크 기능 추가

--- js/tab.js
+++ js/tab.js
@@ -1,10 +1,41 @@
-$('.tab-link').click(function () {
-    var tab_id = $(this).attr('data-tab');
+/* ========= 탭 메뉴 , 외부 링크 ========== */
+let tabLink = $('.tabBox p');
+let tabContent = $('.tab-content');
+let subLink = $('.submenu li');
+let currentUrl = location.href;
 
-    $('.tab-link').removeClass('current');
-    $('.tab-content').removeClass('current');
+tabLink.add(subLink).click(function(e){
+  e.preventDefault();
+  let targetIdx = $(this).index();
 
-    $(this).addClass('current');
-    $("#" + tab_id).addClass('current');
-
+  activateTab(targetIdx)
 });
+
+
+subLink.each(function(i){
+    let compareUrl = $(this).find('a').attr('href');
+    let active = currentUrl.indexOf(compareUrl);
+    let blank = currentUrl.indexOf('#');
+
+    console.log(active)
+
+    if(active > -1){
+        activateTab(i);
+    }
+    if(blank == -1){
+        activateTab(0);
+    }
+});
+
+
+function activateTab(idx){
+    tabContent.hide();
+    tabLink.removeClass('current');
+    tabLink.eq(idx).addClass('current');
+    tabContent.eq(idx).show();
+  } 
+
+  /* 
+    1.매개변수로 숫자가 들어오면 모든 컨텐츠를 안 보이게 함.
+    2.클릭한 요소의 인덱스 번호에 해당하는 것을 보여준다
+  */
